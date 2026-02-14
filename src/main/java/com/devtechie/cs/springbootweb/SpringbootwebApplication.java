d826df9b582fc63fd74a69335b7a8d681230f0f6
debmalyap <debmalyapaulfbp@gmail.com>
2026-02-14 16:49:03 +0530

    Service updated and committed

--- src/main/java/com/devtechie/cs/springbootweb/SpringbootwebApplication.java
+++ src/main/java/com/devtechie/cs/springbootweb/SpringbootwebApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootwebApplication {
-
+public class SpringbootwebApplication 
+{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootwebApplication.class, args);
 	}
